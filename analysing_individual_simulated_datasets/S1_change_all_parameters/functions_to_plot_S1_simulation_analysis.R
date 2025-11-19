df04fe65730586da4a10f79174958583f925bf31
David Le <dle@nid002027.hostmgmt.cm.setonix.pawsey.org.au>
2025-11-20 07:35:41 +0800

    Update plot aesthetics, and included wilcoxon p-value in first scatter plot.

--- analysing_individual_simulated_datasets/S1_change_all_parameters/functions_to_plot_S1_simulation_analysis.R
+++ analysing_individual_simulated_datasets/S1_change_all_parameters/functions_to_plot_S1_simulation_analysis.R
@@ -1,10 +1,14 @@
 # Libraries ----
 library(cowplot)
 library(ggplot2)
+library(S4Vectors)
+library(stringr)
+library(dplyr)
 
 # Read data and set up ----
 setwd("~/R/spaSim-3D_SPIAT-3D_analysing_individual_datasets/analysing_individual_simulated_datasets/S1_change_all_parameters")
-metric_df_list <- readRDS("S1_metric_df_list_5001_to_10000.RDS")
+
+metric_df_list <- readRDS("S1_metric_df_list.RDS")
 
 file_name <- "S1_plots.pdf"
 
@@ -14,6 +18,7 @@ file_name <- "S1_plots.pdf"
 plot_2D_vs_3D_by_metric_and_pair_scatter_plot <- function(metric_df_list,
                                                           metrics) {
   plot_df <- data.frame()
+  pval_df <- data.frame()
   
   for (metric in metrics) {
     
@@ -21,9 +26,8 @@ plot_2D_vs_3D_by_metric_and_pair_scatter_plot <- function(metric_df_list,
     metric_df <- metric_df_list[[metric]]
     
     # Modify the metric_df so that there is a column of 3D values, and a column of 2D values
-    # 2D values are in the second half of the metric_df
-    metric_values2D <- metric_df[[metric]][((nrow(metric_df) / 2 ) + 1):nrow(metric_df)]
-    metric_df <- metric_df[1:(nrow(metric_df) / 2), ]
+    metric_values2D <- metric_df[[metric]][metric_df[["dimension"]] == "2D"]
+    metric_df <- metric_df[metric_df[["dimension"]] == "3D", ]
     metric_df[["value2D"]] <- metric_values2D
     colnames(metric_df)[colnames(metric_df) == metric] <- "value3D"
     metric_df[["dimension"]] <- NULL
@@ -46,12 +50,47 @@ plot_2D_vs_3D_by_metric_and_pair_scatter_plot <- function(metric_df_list,
     # Add metric_df to plot_df
     plot_df <- rbind(plot_df, metric_df[ , c("value3D", "value2D", "pair", "metric")])
     
+    # Add to pval_df
+    p_values <- c()
+    for (pair in unique(metric_df$pair)) {
+      
+      # Ignore when pair is invalid
+      if (sum(is.finite(metric_df[["value3D"]][metric_df$pair == pair])) == 0) {
+        next
+      } 
+      
+      wilcox_test  <- wilcox.test(metric_df[["value3D"]][metric_df$pair == pair], 
+                                  metric_df[["value2D"]][metric_df$pair == pair], 
+                                  paired = TRUE)
+      p_value <- wilcox_test$p.value
+      
+      # Format p-value
+      formatCustomSci <- function(x) {
+        x_sci <- str_split_fixed(formatC(x, format = "e"), "e", 2)
+        alpha <- round(as.numeric(x_sci[ , 1]), 1)
+        power <- as.integer(x_sci[ , 2])
+        paste(alpha, power, sep = "e")
+      }
+      if (p_value == 0) p_value <- 2.2e-308
+      if (0 < p_value && p_value < 1e-3)  {
+        p_value <- formatCustomSci(p_value)
+      }
+      else {
+        p_value  <- round(p_value, 3)
+      }
+      
+      p_values <- c(p_values, p_value)
+    }
+    pval_df <- rbind(pval_df, 
+                     data.frame(metric = metric,
+                                pair = unique(metric_df$pair),
+                                p_value = p_values))
   }
   
   pairs <- unique(plot_df$pair)
-  
+
   fig <- ggplot(plot_df, aes(x = value3D, y = value2D)) +
-    geom_point(alpha = 0.5, color = "#0062c5") +
+    geom_point(alpha = 0.25, color = "#0062c5", size = 1) +
     geom_abline(slope = 1, intercept = 0, linetype = "dotted", color = "#bb0036", linewidth = 1) + # Dotted line of the equation y = x
     labs(title = "Scatterplots showing 2D vs 3D, for each Metric and Pair",
          x = "3D value",
@@ -60,10 +99,20 @@ plot_2D_vs_3D_by_metric_and_pair_scatter_plot <- function(metric_df_list,
     theme_minimal() +
     theme(
       panel.border = element_rect(color = "black", fill = NA, linewidth = 1),
-      axis.text.x = element_blank(),            # remove x-axis numbers
-      axis.text.y = element_blank(),            # remove y-axis numbers
-      axis.ticks.x = element_blank(),           # remove x-axis ticks
-      axis.ticks.y = element_blank()
+      axis.text.x = element_text(size = 4),  # make x-axis text smaller
+      axis.text.y = element_text(size = 4)   # make y-axis text smaller
+    ) +
+    # Add p-value text
+    geom_text(
+      data = pval_df,
+      aes(
+        x = Inf, y = -Inf,   # bottom-right corner
+        label = paste0("p: ", p_value)
+      ),
+      inherit.aes = FALSE,
+      hjust = 1.1, vjust = -0.5,
+      size = 2,
+      color = "black"
     )
   
   return(fig)
@@ -79,9 +128,8 @@ plot_error_vs_3D_by_metric_and_pair_scatter_plot <- function(metric_df_list,
     metric_df <- metric_df_list[[metric]]
     
     # Modify the metric_df so that there is a column of 3D values, and a column of 2D values
-    # 2D values are in the second half of the metric_df
-    metric_values2D <- metric_df[[metric]][((nrow(metric_df) / 2 ) + 1):nrow(metric_df)]
-    metric_df <- metric_df[1:(nrow(metric_df) / 2), ]
+    metric_values2D <- metric_df[[metric]][metric_df[["dimension"]] == "2D"]
+    metric_df <- metric_df[metric_df[["dimension"]] == "3D", ]
     metric_df[["value2D"]] <- metric_values2D
     colnames(metric_df)[colnames(metric_df) == metric] <- "value3D"
     metric_df[["dimension"]] <- NULL
@@ -112,15 +160,18 @@ plot_error_vs_3D_by_metric_and_pair_scatter_plot <- function(metric_df_list,
   pairs <- unique(plot_df$pair)
   
   fig <- ggplot(plot_df, aes(x = value3D, y = error)) +
-    geom_point(alpha = 0.5, color = "#0062c5") +
+    geom_point(alpha = 0.25, color = "#0062c5", size = 1) +
     geom_hline(yintercept = 0, color = "#bb0036", linetype = "dotted", linewidth = 1) + # Red dotted line at y = 0
     labs(title = "Scatterplots showing Error vs 3D, for each Metric and Pair",
          x = "3D value",
          y = "Error (%)") +
-    facet_grid(pair ~ metric, scales = "free") +
+    facet_wrap(~ interaction(metric, pair), scales = "free", nrow = length(pairs), ncol = length(metrics)) +  
+    scale_y_continuous(limits = c(-100, 500)) +
     theme_minimal() +
     theme(
-      panel.border = element_rect(color = "black", fill = NA, linewidth = 1)
+      panel.border = element_rect(color = "black", fill = NA, linewidth = 1),
+      axis.text.x = element_text(size = 6),  # make x-axis text smaller
+      axis.text.y = element_text(size = 6)   # make y-axis text smaller
     )
   
   return(fig)
@@ -136,9 +187,8 @@ plot_error_vs_3D_by_metric_and_pair_box_plot <- function(metric_df_list,
     metric_df <- metric_df_list[[metric]]
     
     # Modify the metric_df so that there is a column of 3D values, and a column of 2D values
-    # 2D values are in the second half of the metric_df
-    metric_values2D <- metric_df[[metric]][((nrow(metric_df) / 2 ) + 1):nrow(metric_df)]
-    metric_df <- metric_df[1:(nrow(metric_df) / 2), ]
+    metric_values2D <- metric_df[[metric]][metric_df[["dimension"]] == "2D"]
+    metric_df <- metric_df[metric_df[["dimension"]] == "3D", ]
     metric_df[["value2D"]] <- metric_values2D
     colnames(metric_df)[colnames(metric_df) == metric] <- "value3D"
     metric_df[["dimension"]] <- NULL
@@ -170,22 +220,16 @@ plot_error_vs_3D_by_metric_and_pair_box_plot <- function(metric_df_list,
   
   fig <- ggplot(plot_df, aes(x = metric, y = error, color = pair)) +
     geom_boxplot(outlier.shape = NA, fill = "lightgray") +  # Hide default outliers to avoid duplication
-    geom_jitter(width = 0.2, alpha = 0.5, aes(color = pair)) +  # Add dots with slight horizontal jitter
     geom_hline(yintercept = 0, color = "#bb0036", linetype = "dotted", linewidth = 1) + # Red dotted line at y = 0
     labs(title = "Error Distribution by Metric, showing Error for each Pair",
          x = "Metric",
          y = "Error (%)") +
+    scale_y_continuous(limits = c(-100, 300)) +
     theme_minimal() +
     theme(
       panel.border = element_rect(color = "black", fill = NA, linewidth = 1)
     ) +
     scale_color_manual(values = c(
-      "A/A" = "#bb0036",
-      "A/B" = "#0062c5",
-      "B/A" = "#007128",
-      "B/B" = "#f77e3b"
-    )) +
-    scale_fill_manual(values = c(
       "A/A" = "#bb0036",
       "A/B" = "#0062c5",
       "B/A" = "#007128",
@@ -196,19 +240,33 @@ plot_error_vs_3D_by_metric_and_pair_box_plot <- function(metric_df_list,
 }
 
 
+
+
 # Running the functions ----
+
+# # Subset...
+# random_numbers <- sample(1:40000, 1000, replace = FALSE)
+# metric_df_list_subset <- list()
+# for (metric in names(metric_df_list)) {
+#   metric_df <- metric_df_list[[metric]]
+#   metric_df_subset <- metric_df[c(random_numbers, random_numbers + 40000), ]
+#   metric_df_list_subset[[metric]] <- metric_df_subset
+# }
 
 metrics <- c("AMD", "ACIN_AUC", "ACINP_AUC", "AE_AUC", "MS_AUC", "NMS_AUC", "CKR_AUC", "CLR_AUC", "COO_AUC", "CGR_AUC", "PBSAC", "PBP_AUC", "EBSAC", "EBP_AUC")
 
 
 fig_2D_vs_3D_by_metric_and_pair_scatter_plot <- plot_2D_vs_3D_by_metric_and_pair_scatter_plot(metric_df_list,
-                                                                                          metrics)
+                                                                                              metrics)
 
 fig_error_vs_3D_by_metric_and_pair_scatter_plot <- plot_error_vs_3D_by_metric_and_pair_scatter_plot(metric_df_list,
-                                                                                               metrics)
+                                                                                                    metrics)
 
 fig_error_vs_3D_by_metric_and_pair_box_plot <- plot_error_vs_3D_by_metric_and_pair_box_plot(metric_df_list,
                                                                                             metrics)
+
+
+
 
 setwd("~/R/plots/S1")
 pdf(file_name, width = 18, height = 10)
