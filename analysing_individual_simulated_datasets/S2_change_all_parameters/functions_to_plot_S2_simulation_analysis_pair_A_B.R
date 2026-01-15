27d9469975e204e6102e292ad2ae06b4ea65d8dd
David Le <dle@nid001709.hostmgmt.cm.setonix.pawsey.org.au>
2026-01-15 16:52:10 +0800

    New functions to plot box plots with correlation for percentage differences for three slices and random slice showing structure.

--- analysing_individual_simulated_datasets/S2_change_all_parameters/functions_to_plot_S2_simulation_analysis_pair_A_B.R
+++ analysing_individual_simulated_datasets/S2_change_all_parameters/functions_to_plot_S2_simulation_analysis_pair_A_B.R
@@ -12,7 +12,7 @@ setwd("~/R/S2_data")
 
 metric_df_list <- readRDS("S2_metric_df_list.RDS")
 
-parameters_df <- readRDS("parameters_df.RDS")
+parameters_df <- readRDS("S2_parameters_df.RDS")
 
 # Functions ----
 
@@ -1034,6 +1034,164 @@ plot_percentage_difference_vs_3D_by_metric_and_pair_A_B_for_three_slices_scatter
   return(fig)
 }
 
+# Plot percentage difference vs metric for each pair for 3 slices
+plot_percentage_difference_vs_metric_by_pair_A_B_for_three_slice_box_plot <- function(metric_df_list,
+                                                                                      metrics) {
+  plot_df <- data.frame()
+  
+  for (metric in metrics) {
+    
+    # Get metric_df for current metric
+    metric_df <- metric_df_list[[metric]]
+    
+    # Change slice column to numeric for safety
+    metric_df$slice <- as.numeric(metric_df$slice)
+    
+    # Add pair column
+    if (metric %in% c("EBSAC", "EBP_AUC")) {
+      # For EBSAC and EBP_AUC, assume pair is the same as cell_types for consistency
+      metric_df$pair <- gsub(',', '/', metric_df$cell_types)
+    }
+    else if (metric %in% c("ANE_AUC")) {
+      # For ANE_AUC, assume pair is the same as target_cell_type for consistency (as target is of form A,B already)
+      metric_df$pair <- gsub(',', '/', metric_df$target)
+    }
+    else {
+      # Add reference-target column
+      metric_df$pair <- paste(metric_df$reference, metric_df$target, sep = "/")
+    }
+    
+    # Subset for A/B pair
+    metric_df <- metric_df[metric_df$pair == "A/B", ]
+    
+    # Modify the metric_df so that there is a column of 3D values, and a column of 2D values
+    colnames(metric_df)[colnames(metric_df) == metric] <- "metric_value"
+    metric_df$sim_pair <- paste(metric_df$simulation, metric_df$pair, sep = "_")
+    
+    metric_df3D <- metric_df[metric_df[["slice"]] == 0, ]
+    colnames(metric_df3D)[colnames(metric_df3D) == "metric_value"] <- "value3D"
+    
+    # Take 3 slices for each simulation and pair to get 2D values
+    # Slice indices 7, 10, 13 correspond with 145, 175, 205 z coord
+    metric_df2D <- metric_df[metric_df[["slice"]] %in% c(7, 10, 13), ] 
+    
+    # Merge metric_df3D and metric_df2D
+    metric_df <- metric_df2D %>% left_join(metric_df3D[, c("sim_pair", "value3D")], by = "sim_pair")
+    colnames(metric_df)[colnames(metric_df) == "metric_value"] <- "value2D"
+    
+    metric_df$metric <- metric  
+    
+    # Add metric_df to plot_df
+    plot_df <- rbind(plot_df, metric_df[ , c("value3D", "value2D", "pair", "metric", "slice")])
+    
+  }
+  
+  # Change slice column back to character for plotting
+  plot_df$slice <- as.character(metric_df$slice)
+  
+  plot_df$slice <- factor(plot_df$slice, levels = c('7', '10', '13'))
+  
+  # Get error column
+  plot_df$error <- (plot_df$value2D - plot_df$value3D) / plot_df$value3D * 100
+  
+  # Get correlation
+  corr_df <- plot_df %>% 
+    group_by(metric, slice) %>% 
+    summarise(
+      corr = cor(value3D, value2D, method = "spearman", use = "complete.obs"), 
+      .groups = "drop")
+  
+  write.csv(corr_df, "~/R/values_from_figures/three_slices_corr_df.csv")
+  
+  # map corr ∈ [-1,1] → error ∈ [-100,400]
+  corr_to_error <- function(c) scales::rescale(c, to = c(-100, 400), from = c(0, 1))
+  
+  # inverse transform for the secondary axis
+  error_to_corr <- function(e) scales::rescale(e, to = c(0, 1), from = c(-100, 400))
+  
+  corr_df$y_trans <- corr_to_error(corr_df$corr)
+  
+  # For nicer tick labels
+  sci_clean_threshold <- function(x) {
+    sapply(x, function(v) {
+      if (is.na(v)) return("")
+      
+      if (abs(v) < 1000) {
+        return(as.character(v))
+      }
+      
+      # scientific notation with 1 decimal place
+      s <- formatC(v, format = "e", digits = 1)   # e.g. "1.5e+03"
+      
+      # remove "+" in exponent
+      s <- gsub("e\\+", "e", s)
+      
+      # split mantissa and exponent
+      parts <- strsplit(s, "e")[[1]]
+      mant <- parts[1]
+      exp  <- parts[2]
+      
+      # remove trailing .0 (so 1.0e3 → 1e3)
+      mant <- sub("\\.0$", "", mant)
+      
+      # remove leading zeros in exponent
+      exp <- sub("^0+", "", exp)
+      
+      paste0(mant, "e", exp)
+    })
+  }
+  
+  # Define slice colors
+  slice_cols <- c(
+    "7"  = "#9437a8",
+    "10" = "#007128",
+    "13" = "#b8db50"
+  )
+  
+  
+  fig <- ggplot(plot_df, aes(x = metric, y = error, fill = slice)) +
+    geom_boxplot(position = position_dodge(width = 0.8)) +
+    scale_fill_manual(values = slice_cols) +
+    geom_hline(yintercept = 0, color = "#bb0036", linetype = "dotted", linewidth = 1) + # Red dotted line at y = 0
+    
+    # Median labels
+    stat_summary(
+      fun = function(z) median(z, na.rm = TRUE),
+      geom = "text",
+      aes(label = after_stat(sprintf("%.1f", y)), group = slice),
+      position = position_dodge(width = 0.8),
+      vjust = -0.5,
+      size = 3.5,
+      color = "black"
+    ) +
+  
+    
+    # ⭐ correlation stars
+    geom_point(
+      data = corr_df,
+      aes(x = metric, y = y_trans, group = slice),
+      shape = 8,
+      size = 5,
+      color = "#0062c5",
+      position = position_dodge(width = 0.8)
+    ) +
+  
+    
+    labs(title = "Box plots showing percentage difference between 2D and 3D metrics with spearman correlation, for three slices for cell pair A/B",
+         x = "Metric",
+         y = "Percentage difference (%)") +
+    
+    scale_y_continuous(limits = c(-100, 400), n.breaks = 3, labels = sci_clean_threshold,
+                       sec.axis = sec_axis(~ error_to_corr(.), name = "Spearman Correlation")) +
+    
+    theme_minimal() +
+    theme(
+      panel.border = element_rect(color = "black", fill = NA, linewidth = 1)
+    )
+  
+  return(fig)
+}
+
 
 
 # Plot 2D vs 3D for each metric and pair for a random slice, annotating for tissue structure
@@ -1341,17 +1499,6 @@ plot_2D_vs_3D_correlation_vs_structure_by_metric_and_pair_A_B_for_random_slice_b
     
   }
   
-  # Compute spearman correlation
-  corr_df <- plot_df %>%
-    group_by(pair, metric, structure) %>%
-    summarise(
-      spearman_corr = cor(value3D, value2D, method = "spearman", use = "complete.obs"),
-      .groups = "drop"
-    )
-  
-  write.csv(corr_df, "~/R/values_from_figures/corr_df.csv")
-  
-  
   fig <- ggplot(corr_df, aes(x = structure, y = spearman_corr, fill = structure)) + 
     geom_col(alpha = 0.8) + 
     labs(title = "Bar plots showing spearman correlation vs structure, for each metric, for a random slice and cell pair A/B", x = "Structure", y = "Spearman Correlation") + 
@@ -1369,6 +1516,171 @@ plot_2D_vs_3D_correlation_vs_structure_by_metric_and_pair_A_B_for_random_slice_b
         "separated_ellipsoid" = "#770026",
         "separated_network" = "#bb0036"
       )
+    )
+  
+  return(fig)
+}
+
+# Plot percentage difference vs metric for each pair for a random slice, annotating for tissue structure
+plot_percentage_difference_vs_metric_by_pair_A_B_for_random_slice_showing_structure_box_plot <- function(metric_df_list,
+                                                                                                         metrics,
+                                                                                                         parameters_df) {
+  plot_df <- data.frame()
+  
+  for (metric in metrics) {
+    
+    # Get metric_df for current metric
+    metric_df <- metric_df_list[[metric]]
+    
+    # Change slice column to numeric for safety
+    metric_df$slice <- as.numeric(metric_df$slice)
+    
+    if (metric %in% c("EBSAC", "EBP_AUC")) {
+      # For EBSAC and EBP_AUC, assume pair is the same as cell_types for consistency
+      metric_df$pair <- gsub(',', '/', metric_df$cell_types)
+    }
+    else if (metric %in% c("ANE_AUC")) {
+      # For ANE_AUC, assume pair is the same as target_cell_type for consistency (as target is of form A,B already)
+      metric_df$pair <- gsub(',', '/', metric_df$target)
+    }
+    else {
+      # Add reference-target column
+      metric_df$pair <- paste(metric_df$reference, metric_df$target, sep = "/")
+    }
+    
+    metric_df$metric <- metric  
+    
+    # Select 3D values, when slice == 0, then remove from data frame
+    metric_values3D <- metric_df[[metric]][metric_df[["slice"]] == 0]
+    metric_df <- metric_df[metric_df[["slice"]] != 0, ]
+    
+    # Select a random 2D slice
+    metric_df <- metric_df %>%
+      group_by(simulation, pair) %>%
+      slice_sample(n = 1) %>%   # pick one random slice per simulation
+      ungroup() %>%
+      select(simulation, pair, metric, value2D = .data[[metric]])
+    
+    # Combine 3D and 2D values
+    metric_df[["value3D"]] <- metric_values3D
+    
+    # Merge metric_df and parameters_df
+    parameters_df$simulation <- seq(nrow(parameters_df))
+    metric_df <- metric_df %>% left_join(parameters_df[, c("simulation", "arrangement", "shape")], by = "simulation")
+    
+    # Get structure column
+    metric_df$structure <- paste(metric_df$arrangement, metric_df$shape, sep = "_")
+    
+    metric_df$metric <- metric  
+    
+    # Subset for A/B pair
+    metric_df <- metric_df[metric_df$pair == "A/B", ]
+    
+    
+    # Add metric_df to plot_df
+    plot_df <- rbind(plot_df, metric_df[ , c("value3D", "value2D", "pair", "metric", "structure")])
+    
+  }
+  
+  # Get error column
+  plot_df$error <- (plot_df$value2D - plot_df$value3D) / plot_df$value3D * 100
+  
+  # Compute spearman correlation
+  corr_df <- plot_df %>%
+    group_by(pair, metric, structure) %>%
+    summarise(
+      corr = cor(value3D, value2D, method = "spearman", use = "complete.obs"),
+      .groups = "drop"
+    )
+  
+  write.csv(corr_df, "~/R/values_from_figures/showing_structure_corr_df.csv")
+  
+  # map corr ∈ [-1,1] → error ∈ [-100,400]
+  corr_to_error <- function(c) scales::rescale(c, to = c(-100, 400), from = c(0, 1))
+  
+  # inverse transform for the secondary axis
+  error_to_corr <- function(e) scales::rescale(e, to = c(0, 1), from = c(-100, 400))
+  
+  corr_df$y_trans <- corr_to_error(corr_df$corr)
+
+  # For nicer tick labels
+  sci_clean_threshold <- function(x) {
+    sapply(x, function(v) {
+      if (is.na(v)) return("")
+      
+      if (abs(v) < 1000) {
+        return(as.character(v))
+      }
+      
+      # scientific notation with 1 decimal place
+      s <- formatC(v, format = "e", digits = 1)   # e.g. "1.5e+03"
+      
+      # remove "+" in exponent
+      s <- gsub("e\\+", "e", s)
+      
+      # split mantissa and exponent
+      parts <- strsplit(s, "e")[[1]]
+      mant <- parts[1]
+      exp  <- parts[2]
+      
+      # remove trailing .0 (so 1.0e3 → 1e3)
+      mant <- sub("\\.0$", "", mant)
+      
+      # remove leading zeros in exponent
+      exp <- sub("^0+", "", exp)
+      
+      paste0(mant, "e", exp)
+    })
+  }
+
+  # Structure colors
+  structure_cols <- c(
+    "mixed_ellipsoid" = "#007128",
+    "mixed_network" = "#b8db50",
+    "ringed_ellipsoid" = "#9437a8",
+    "ringed_network" = "#d99dff",
+    "separated_ellipsoid" = "#770026",
+    "separated_network" = "#bb0036"
+  )
+  
+  fig <- ggplot(plot_df, aes(x = metric, y = error, fill = structure)) +
+    geom_boxplot(position = position_dodge(width = 0.8)) +
+    scale_fill_manual(values = structure_cols) +
+    geom_hline(yintercept = 0, color = "#bb0036", linetype = "dotted", linewidth = 1) + # Red dotted line at y = 0
+    
+    # Median labels
+    stat_summary(
+      fun = function(z) median(z, na.rm = TRUE),
+      geom = "text",
+      aes(label = after_stat(sprintf("%.1f", y)), group = structure),
+      position = position_dodge(width = 0.8),
+      vjust = -0.5,
+      size = 3.5,
+      color = "black"
+    ) +
+    
+    
+    # ⭐ correlation stars
+    geom_point(
+      data = corr_df,
+      aes(x = metric, y = y_trans, group = structure),
+      shape = 8,
+      size = 5,
+      color = "#0062c5",
+      position = position_dodge(width = 0.8)
+    ) +
+    
+    
+    labs(title = "Box plots showing percentage difference between 2D and 3D metrics with spearman correlation, for a random slice for cell pair A/B, showing tissue structure",
+         x = "Metric",
+         y = "Percentage difference (%)") +
+    
+    scale_y_continuous(limits = c(-100, 400), n.breaks = 3, labels = sci_clean_threshold,
+                       sec.axis = sec_axis(~ error_to_corr(.), name = "Spearman Correlation")) +
+    
+    theme_minimal() +
+    theme(
+      panel.border = element_rect(color = "black", fill = NA, linewidth = 1)
     )
   
   return(fig)
@@ -1458,6 +1770,15 @@ pdf("fig_percentage_difference_vs_3D_by_metric_and_pair_A_B_for_three_slices_sca
 print(fig_percentage_difference_vs_3D_by_metric_and_pair_A_B_for_three_slices_scatter_plot)
 dev.off()
 
+setwd("~/R/plots/S2")
+fig_percentage_difference_vs_metric_by_pair_A_B_for_three_slice_box_plot <- plot_percentage_difference_vs_metric_by_pair_A_B_for_three_slice_box_plot(metric_df_list,
+                                                                                                                                                      metrics)
+pdf("fig_percentage_difference_vs_metric_by_pair_A_B_for_three_slice_box_plot.pdf", width = 24, height = 10)
+print(fig_percentage_difference_vs_metric_by_pair_A_B_for_three_slice_box_plot)
+dev.off()
+
+
+
 
 
 
@@ -1488,6 +1809,18 @@ fig_2D_vs_3D_correlation_vs_structure_by_metric_and_pair_A_B_for_random_slice_ba
 pdf("fig_2D_vs_3D_correlation_vs_structure_by_metric_and_pair_A_B_for_random_slice_bar_plot.pdf", width = 24, height = 10)
 print(fig_2D_vs_3D_correlation_vs_structure_by_metric_and_pair_A_B_for_random_slice_bar_plot)
 dev.off()
+
+
+setwd("~/R/plots/S2")
+fig_percentage_difference_vs_metric_by_pair_A_B_for_random_slice_showing_structure_box_plot <- plot_percentage_difference_vs_metric_by_pair_A_B_for_random_slice_showing_structure_box_plot(metric_df_list,
+                                                                                                                                                                                            metrics,
+                                                                                                                                                                                            parameters_df)
+pdf("fig_percentage_difference_vs_metric_by_pair_A_B_for_random_slice_showing_structure_box_plot.pdf", width = 24, height = 10)
+print(fig_percentage_difference_vs_metric_by_pair_A_B_for_random_slice_showing_structure_box_plot)
+dev.off()
+
+
+
 
 
 
